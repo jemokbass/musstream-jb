89ea17294215dc55b8c3cffcf40980130cab5812
jemokbass <jemokbass@gmail.com>
2023-10-16 16:30:07 +0400

    13 - Sound timer with new slider functionality and PlayerContent interval logic were realized.

--- components/Player/Player.tsx
+++ components/Player/Player.tsx
@@ -14,7 +14,7 @@ export const Player = () => {
   }
 
   return (
-    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+    <div className="fixed bottom-0 bg-black w-full h-[83px]">
       <PlayerContent song={song} songUrl={songUrl} />
     </div>
   );
